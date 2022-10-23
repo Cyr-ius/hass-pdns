368c533740253a2254189c1e36a3d953dce65763
cyr-ius <cyr-ius@ipocus.net>
2022-10-23 14:07:32 +0200

    Rename and update extra attribue

--- custom_components/pdns/binary_sensor.py
+++ custom_components/pdns/binary_sensor.py
@@ -8,9 +8,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from . import DOMAIN
-
-CONF_PUBLIC_IP = "public_ip"
+from . import DOMAIN, PDNSDataUpdateCoordinator
 
 
 async def async_setup_entry(
@@ -23,19 +21,26 @@ async def async_setup_entry(
     async_add_entities([DyndnsStatus(coordinator)])
 
 
-class DyndnsStatus(CoordinatorEntity, BinarySensorEntity):
+class DyndnsStatus(CoordinatorEntity[PDNSDataUpdateCoordinator], BinarySensorEntity):
     """Representation of a status sensor."""
 
     _attr_device_class = BinarySensorDeviceClass.PROBLEM
-    _attr_name = "Dynamic Update"
-    _attr_unique_id = "dynamic_update"
+    _attr_has_entity_name = True
 
     def __init__(self, coordinator):
         """Initialize the sensor."""
         super().__init__(coordinator)
-        self._attr_extra_state_attributes = coordinator.data
+        # self._attr_name = coordinator.config_entry.data["dns_alias"]
+        self._attr_name = coordinator.config_entry.title
+        self._attr_unique_id = coordinator.config_entry.entry_id
+        self.coordinator = coordinator
 
     @property
     def is_on(self):
         """Return true if the binary sensor have a trouble."""
         return self.available is False
+
+    @property
+    def extra_state_attributes(self):
+        """Return extra attributes."""
+        return self.coordinator.data
